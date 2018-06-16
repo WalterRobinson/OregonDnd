05232fa5e926faeeca9d35d2ea648f4557de0272
WalterRobinson <33521741+WalterRobinson@users.noreply.github.com>
2018-06-16 14:25:52 -0400

    Update Item.java

--- Item.java
+++ Item.java
@@ -2,15 +2,32 @@ public class Item
 {
 String name;
 int price;
-  
+
+double attackMult;
+double defMult;
+double stressMult;
   
 ArmorType place;
 
-public Item(String n, int p, ArmorType pos)
+public Item(String n, int p, ArmorType pos, double atkm, double defm, double strm)
 {
   name = n;
   price = p;
   place = pos;
+  attackMult = atkm;
+  defMult = defm;
+  stressMult = strm;
 }
-
+public double beforeAttack(){
+ return attackMult; 
+}
+public void afterAttack();
+public double beforeDefend(){
+ return defMult; 
+}
+public void afterDefend();
+public double gainingStress(){
+ return stressMult; 
+}
+  
 }
