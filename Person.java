b85c77fdbce458ba0f9f49f3eb778f73a44c8928
WalterRobinson <33521741+WalterRobinson@users.noreply.github.com>
2018-06-16 14:16:30 -0400

    Update Person.java

--- Person.java
+++ Person.java
@@ -9,13 +9,14 @@ Role r;
 Weapon w;
 Item headspot;
 Item chestspot;
+Item handspot;
 Item legspot;
 Item footspot;
 Item beltspot;
 Item ring1;
 Item ring2
-Item handspot;
-  
+
+int stressLevel=0;
   
 Public Person(String n, Role ro, int h)
 {
