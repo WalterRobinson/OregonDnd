59737db276fb419b5d1fd0dace7778cacb5d42b2
WalterRobinson <33521741+WalterRobinson@users.noreply.github.com>
2018-07-16 21:33:45 -0400

    Update Status.java

--- Status.java
+++ Status.java
@@ -1,9 +1,9 @@
 public class Status
 {
 String name;
-int timeLimit;//how long will the status last
+int timeLimit;//how long will the status last, 0 to not do anything, -1 to last forever
 int atkMult;//.5 for slow, 0 for frozen
-int damOverTime;
+int damOverTime;//damage per tick, 0 for no damage over time, negative for heal
 int count=0;
 int damageTime; //when the damage over time occurs, do mod to find when this happens
 
@@ -15,4 +15,16 @@ atkMult=aM;
 damOverTime=dOT;
 damageTime=dT;
 }
+public tick(Person p);
+  {
+   count++;
+   if(count%damageTime==0)
+   {
+     p.takeDamage(damOverTime);
+   }
+   if(count%timeLimit==0)
+   {
+     p.removeStatus(this);
+   }
+  }
 }
