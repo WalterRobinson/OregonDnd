25c7aa3bf6be8728e0b8612dde03c114b2d367bd
WalterRobinson <33521741+WalterRobinson@users.noreply.github.com>
2018-07-16 21:16:46 -0400

    Update Person.java

--- Person.java
+++ Person.java
@@ -8,6 +8,11 @@ int level=1;
 int expNecessary=100;
 int currExp=0;
 
+int Atk=10;
+int Mtk=10;
+int Def=10;
+int Mef=10;
+
 Role r;
 Weapon w;
 Item headspot;
@@ -21,12 +26,16 @@ Item ring2
 
 int stressLevel=0;
   
-Public Person(String n, Role ro, int h)
+public Person(String n, Role ro, int h)
 {
 name = n;
 r = ro;
 maxHealth = h;
 currHealth = maxHealth;
 w = new Weapon("Fists", 5, 20, 0, false, false, null);
+}
+public Attack()
+{
+  
 }
 }
