e5594e4a92383129a7b592d4b3c93841c7e174bd
WalterRobinson <33521741+WalterRobinson@users.noreply.github.com>
2018-07-16 22:26:10 -0400

    Update Weapon.java

--- Weapon.java
+++ Weapon.java
@@ -7,8 +7,10 @@ public class Weapon
   int price;
   boolean isMagic;
   boolean isRanged;
-  Status possStatus;
-  public Weapon(String n, int a, int c, int p, boolean isM, boolean isR, Status stat)
+  Status possEnemyStatus;
+  double enemyStatusChance;//chance that enemy receives status, less than 1
+  Status possSelfStatus;
+  public Weapon(String n, int a, int c, int p, boolean isM, boolean isR, Status statE, double statusChance, Status statS)
   {
     name = n;
     atkdam = a;
@@ -16,7 +18,30 @@ public class Weapon
     price = p;
     isMagic=isM;
     isRanged=isR;
-    possStatus=stat;
+    possEnemyStatus=statE;
+    enemyStatusChance=statusChance;
+    possSelfStatus=statS;
   }
-
+  public Attack(Person p, Person e)
+  {
+    double multiplier = p.getMultiplier();
+    if(isMagic)
+    {
+      e.takeDamage( (int)( ( ( (double)(30+p.Mtk) )/(30+e.Mef) )*atkdam*multiplier ) +1 ) 
+    }
+    else
+    {
+      e.takeDamage( (int)( ( ( (double)(30+p.Atk) )/(30+e.Def) )*atkdam*multiplier ) +1 )
+    }
+    if(possEnemyStatus!=null){
+     if(Math.random()<enemyStatusChance)
+     {
+      e.addStatus(new Status(possEnemyStatus.name, possEnemyStatus.timeLimit , possEnemyStatus.atkMult , possEnemyStatus.damOverTime , possEnemyStatus.damageTime )); 
+     }
+    }
+    if(possSelfStatus!=null)
+    {
+    p.addStatus(new Status(possSelfStatus.name, possSelfStatus.timeLimit , possSelfStatus.atkMult , possSelfStatus.damOverTime , possSelfStatus.damageTime));
+    }
+  }
 }
