f845be6f8d2dcc159cd3435eeb6d58f1878b318b
WalterRobinson <33521741+WalterRobinson@users.noreply.github.com>
2018-07-17 19:47:50 -0400

    Update Encounter.java

--- Encounter.java
+++ Encounter.java
@@ -8,8 +8,9 @@ public class Encounter
     ArrayList<Object> payout=new ArrayList();//could be weapons, armor, or even new people
     ArrayList<Integer> money=new ArrayList();//so if buying, selling, or finding money, positive number means money gain, make sure if money is negative and more than player has, put a rejection message
     ArrayList<int[]> statChange=new ArrayList();//order is Atk,Mtk,Def,Mef. positive number means stat increase
+    ArrayList<Encounter> followingEncounters = new ArrayList();
     
-    public Encounter(String t, String[] o, int[] d, int[] s, Object[] p, int[] m, int[][] stats)
+    public Encounter(String t, String[] o, int[] d, int[] s, Object[] p, int[] m, int[][] stats, ArrayList<Encounter> fE)
     {
         text=t;
         for(int i = 0; i<o.length;i++)
@@ -20,6 +21,7 @@ public class Encounter
             payout.add(p[i].makeCopy());
             money.add(m[i]);
             statChange.add(stats[i]);
+            followingEncounters.add(fE.get(i));
         }
         
     }
